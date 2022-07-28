b5b95b80c1936a3c3d5c6f84cede4903bb8cca88
VanessaFerraz <54750913+VanessaFerraz@users.noreply.github.com>
2022-07-28 19:06:03 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Profa Vanessa Ferraz</title>
+    </head>
+    <body>
+    
+        <h1> Profa. Vanessa Ferraz </h1>
+        <br>
+        <h1> Arquivos das aulas de PHP </h1>
+
+        <ul>
+            <li> <a href="cookie-cor/index.php"> Cookie - mudar cor de fundo </a> </li>
+            <li> <a href="calculo-rodizio/index.php"> Cálculo do rodízio de um veículo </a> </li>
+        </ul>
+    </body>
+</html>
